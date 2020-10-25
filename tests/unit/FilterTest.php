02adce4f734d24e5285485a5841338529fd7354b
Miguel Ángel Viera <mangelviera93@gmail.com>
2020-10-25 23:23:19 +0000

    Refactor: remove PHPDoc exception not longer thrown

--- tests/unit/FilterTest.php
+++ tests/unit/FilterTest.php
@@ -5,15 +5,12 @@ namespace tests\unit;
 
 
 use Codesai\Collections\Collection;
-use Codesai\Collections\exceptions\InfiniteCollectionValuesNotBounded;
 use PHPUnit\Framework\TestCase;
 
 class FilterTest extends TestCase
 {
 
-    /** @test
-     * @throws InfiniteCollectionValuesNotBounded
-     */
+    /** @test */
     public function filter_values_of_an_array_collection_for_a_given_lambda()
     {
         $givenCollection = Collection::from([1, 2, 3, 4, 5, 6]);
@@ -23,9 +20,7 @@ class FilterTest extends TestCase
         self::assertEquals([0 => 2, 1 => 4, 2 => 6], $result);
     }
 
-    /** @test
-     * @throws InfiniteCollectionValuesNotBounded
-     */
+    /** @test */
     public function filter_values_of_a_map_collection_for_a_given_lambda()
     {
         $givenCollection = Collection::from(['parrot' => 1, 'cockatoo' => 2, 'african_grey' => 3]);
